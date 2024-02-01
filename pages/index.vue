f2739b4598563c908cbd0340df173413d625d62a
odilov-a <akbarjonodilov01@gmail.com>
2024-02-01 21:58:27 +0500

    front end

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,43 @@
 <template>
-  <div>
-    <h1>Todo API</h1>
-    <ul>
-      <li v-for="item in apiData" :key="item._id">
-        {{ item.task }} - {{ item.description }} - {{ item.completed }}
-        <button @click="editTodo(item)">Edit</button>
-        <button @click="deleteTodo(item._id)">Delete</button>
-      </li>
-    </ul>
+  <div class="container mx-auto p-6">
+    <h1 class="text-3xl font-bold mb-6">Todo API</h1>
+    <table class="min-w-full border border-slate-500">
+      <thead>
+        <tr>
+          <th class="py-2 px-4 border border-slate-600">Status</th>
+          <th class="py-2 px-4 border border-slate-600">Task</th>
+          <th class="py-2 px-4 border border-slate-600">Description</th>
+          <th class="py-2 px-4 border border-slate-600">Actions</th>
+          <th class="py-2 px-4 border border-slate-600">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in apiData" :key="item._id">
+          <td class="py-2 px-4 border border-slate-700">
+            <input type="checkbox" :checked="item.completed" @change="updateStatus(item)" :disabled="item.completed" class="form-checkbox text-blue-500"/>
+          </td>
+          <td class="py-2 px-4 border border-slate-700">{{ item.task }}</td>
+          <td class="py-2 px-4 border border-slate-700">{{ item.description }}</td>
+          <td class="py-2 px-4 border border-slate-700">
+            <button @click="editTodo(item)" class="bg-blue-500 text-white px-4 py-1 rounded">Edit</button>
+          </td>
+          <td class="py-2 px-4 border border-slate-700">
+            <button @click="deleteTodo(item._id)" class="bg-red-500 text-white px-4 py-1 rounded">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
-    <form @submit.prevent="addOrUpdateTodo">
-      <label>Task: </label>
-      <input v-model="newTodo.task" required />
-      <label>Description: </label>
-      <input v-model="newTodo.description" required />
-      <label>Completed: </label>
-      <input type="checkbox" v-model="newTodo.completed" />
-      <button type="submit">{{ editingTodo ? "Update" : "Add" }} Todo</button>
+    <form @submit.prevent="addOrUpdateTodo" class="mt-4">
+      <label class="block mb-2">Task:</label>
+      <input v-model="newTodo.task" required class="border border-slate-600 px-3 py-2 mb-2" />
+      <label class="block mb-2">Description:</label>
+      <input v-model="newTodo.description" required class="border border-slate-600 px-3 py-2 mb-2" />
+      <label class="block mb-2">Completed:</label>
+      <input type="checkbox" v-model="newTodo.completed" class="mr-2" />
+      <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded mt-4">
+        {{ editingTodo ? "Update" : "Add" }} Todo
+      </button>
     </form>
   </div>
 </template>
@@ -58,6 +79,15 @@ const addOrUpdateTodo = async () => {
 const editTodo = (todo) => {
   editingTodo.value = todo;
   newTodo.value = { ...todo };
+};
+
+const updateStatus = async (task) => {
+  try {
+    await axios.put(`http://localhost:8080/api/todos/${task._id}`, { completed: !task.completed });
+    fetchTodos();
+  } catch (error) {
+    console.error("Error updating status:", error);
+  }
 };
 
 const deleteTodo = async (todoId) => {
